0ea02ca9981edf6ee344409c549c4a54fd02628b
Poonam-gaian <poonam.r@gaiansolutions.com>
2024-08-29 19:09:01 +0530

    added the hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import HeroSection from "@/Molecules/HeroSection";
 import React from "react";
 
 function Home() {
-	return <div></div>;
+	return (
+		<div className="w-full">
+			<HeroSection />
+		</div>
+	);
 }
 
 export default Home;
