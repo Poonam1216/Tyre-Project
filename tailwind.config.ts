628b517ee9e71abe299a7999c91b658142655b71
Poonam-gaian <poonam.r@gaiansolutions.com>
2024-08-30 11:26:09 +0530

    added the offer ribbon

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
-import type { Config } from "tailwindcss";
+import { Config } from "tailwindcss";
 
-const config = {
+const config: Config = {
 	darkMode: ["class"],
 	content: [
 		"./pages/**/*.{ts,tsx}",
@@ -67,14 +67,30 @@ const config = {
 					from: { height: "var(--radix-accordion-content-height)" },
 					to: { height: "0" },
 				},
+				"ribbon-scroll-left": {
+					"100%": { transform: "translateX(-01%)" },
+					"0%": { transform: "translateX(0%)" },
+				},
+				"ribbon-scroll-right": {
+					"0%": { transform: "translateX(-01%)" },
+					"100%": { transform: "translateX(0%)" },
+				},
 			},
 			animation: {
 				"accordion-down": "accordion-down 0.2s ease-out",
 				"accordion-up": "accordion-up 0.2s ease-out",
+				"g-scroll-left": "ribbon-scroll-left 2000s linear infinite",
+				"g-scroll-right": "ribbon-scroll-right 2000s linear infinite",
+			},
+			backgroundImage: {
+				"gradient-ribbon-1":
+					"linear-gradient(90deg, #F8463F 0%, #F1DEC6 100%)",
+				"gradient-ribbon-2":
+					"linear-gradient(90deg, #FABC3F 0%, #F8463F 100%)",
 			},
 		},
 	},
 	plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+};
 
 export default config;
